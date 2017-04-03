7fcedc61af28059a155279a60d6f65c6da0bf5f4
Manca Florimond <florimond.manca@student.ecp.fr>
2017-04-03 19:40:21 +0200

    Test with 1000 particles in a 500x500 box, seems slow. Seems due to matplotlib, will try pygame

--- src/main.py
+++ src/main.py
@@ -2,34 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.spatial.distance import cdist
+from time import time
 
 
-NPARTICLES = 100
-L = 100
-AGGR_R = 3  # aggregation radius
+NPARTICLES = 1000
+L = 500
+AGGR_R = 7  # aggregation radius
 CENTER = np.ones((1, 2)) * L / 2
 
 
-def randvel(aggr, temp=10):
+def randvel(aggr, temp=5):
     vel = (2 * np.random.random((NPARTICLES, 2)) - 1) * temp
     vel[aggr] = 0  # aggregated particles don't move anymore
     return vel
 
 
 def move(particles, aggr):
+    # generate new velocities
+    particles['vel'] = randvel(aggr)
+    # move
     particles['pos'] += particles['vel']
     p = particles['pos']
     # constrain in box
     p[p < 0] += L
     p[p > L] -= L
-    # aggregate to center :
+    # aggregate:
     # 1° compute distances from points to aggregation points
     aggr_points = np.vstack((p[aggr], CENTER))
     dist = cdist(p, aggr_points)
     # 2° aggregate those near to aggregation points
     aggr[np.any(dist < AGGR_R, axis=1)] = True
-    # generate new velocities
-    particles['vel'] = randvel(aggr)
 
 
 def initp():
@@ -54,28 +56,34 @@ def anim():
     ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
                          xlim=(0, L), ylim=(0, L))
     ax.grid()
-    line, = ax.plot([], [], 'bo', lw=2, ms=2 * AGGR_R)
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    moving, = ax.plot([], [], 'bo')
+    fixed, = ax.plot([], [], 'ro')
 
     def init():
         """initialize animation"""
-        line.set_data([], [])
-        return line,
+        moving.set_data([], [])
+        fixed.set_data([], [])
+        return moving, fixed
 
     def animate(i):
         """perform animation step"""
+        t = time()
         move(particles, aggr)
-        x = particles['pos'][:, 0]
-        y = particles['pos'][:, 1]
-        line.set_data(x, y)
-        return line,
+        moving_p = particles['pos'][~aggr]
+        fixed_p = particles['pos'][aggr]
+        moving.set_data(moving_p[:, 0], moving_p[:, 1])
+        fixed.set_data(fixed_p[:, 0], fixed_p[:, 1])
+        print(time() - t)
+        return moving, fixed
 
-    from time import time
     t0 = time()
     animate(0)
     t1 = time()
     interval = 1000 * dt - (t1 - t0)
 
-    ani = animation.FuncAnimation(fig, animate, frames=int(10 / dt),
+    ani = animation.FuncAnimation(fig, animate, frames=int(1 / dt),
                                   interval=interval, blit=False,
                                   init_func=init)
     plt.show()
--- src/scrapbook.py
+++ src/scrapbook.py
@@ -8,10 +8,10 @@ pos = np.random.random((n, 2))
 aggr = np.zeros(n, dtype=np.bool)
 aggr[np.random.random(aggr.shape) < .5] = True
 # calculate distance from center
-d = cdist(pos, np.ones((1, 2)) / 2)[:, 0]
+d = cdist(pos, np.ones((2, 2)) / 2)
+print(d)
 # if inferior to some value, aggregate
-aggr[d < 0.3] = True
+aggr[np.any(d < 0.3, axis=1)] = True
 # velocity generation
 vel = (2 * np.random.random((n, 2)) - 1)
 vel[aggr] = 0
-print(vel)
